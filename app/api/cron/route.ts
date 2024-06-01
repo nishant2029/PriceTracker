d179190f4a858a4cf8423a0afcf241bc1378d5bd
NISHANT KUMAR <nishant7654531965@gmail.com>
2024-06-01 08:40:38 +0530

    fixed deployment errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,6 +4,9 @@ import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET() {
   try {
